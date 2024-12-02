da68f486d85acca3b7a68ffc24ceec157aa43b95
Andruha99 <Andriei.korovin.2014@mail.ru>
2024-12-02 16:36:06 +0300

    Add images

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import productsRouter from "./routes/product";
+import path from "path";
 
 const cors = require("cors");
 
@@ -13,6 +14,8 @@ app.use(cors());
 mongoose.connect(DB_ADDRESS);
 
 app.use(productsRouter);
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
