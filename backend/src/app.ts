8d53bad3998b42b2a2cfb87e5cd1b1468f96ba77
Andruha99 <Andriei.korovin.2014@mail.ru>
2024-12-02 14:45:51 +0300

    Add product schema and model

---
+++ backend/src/app.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import mongoose from "mongoose";
+
+const cors = require("cors");
+
+const { PORT = 3000, DB_ADDRESS = "mongodb://127.0.0.1:27017/weblarek" } =
+  process.env;
+
+const app = express();
+app.use(cors());
+
+mongoose.connect(DB_ADDRESS);
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
